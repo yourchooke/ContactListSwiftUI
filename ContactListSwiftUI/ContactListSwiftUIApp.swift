f2816f1e84bf09013467dca9fb5fca7a72cd7efd
Olga Yurchuk <olga.yourchooke@gmail.com>
2022-11-17 15:35:40 +0700

    Initial Commit

---
+++ ContactListSwiftUI/ContactListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftUIApp.swift
+//  ContactListSwiftUI
+//
+//  Created by Olga Yurchuk on 17.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
